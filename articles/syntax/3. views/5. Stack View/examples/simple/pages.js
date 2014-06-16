88343db3746be96473bf006c2115bbbf2b53c005
Craig Condon <craig@spiceapps.com>
2014-06-16 13:19:32 -0700

    shrinkwrap

--- articles/syntax/3. views/5. Stack View/examples/simple/pages.js
+++ articles/syntax/3. views/5. Stack View/examples/simple/pages.js
@@ -1,11 +1,17 @@
 var views = require("mojo-views");
 
 module.exports = views.Stack.extend({
+
+  // initial state
   state: "home",
+
+  // individual states
   sections: {
     home: require("./home"),
     contact: require("./contact")
   },
+
+  // called by each state
   setPageState: function (state) {
     this.set("state", state);
   }
