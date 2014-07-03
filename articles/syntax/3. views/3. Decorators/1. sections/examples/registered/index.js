60924b104815b3fb2e81ac97e21e888d7a10e5f3
Craig Condon <craig@spiceapps.com>
2014-07-03 12:25:25 -0700

    update

--- articles/syntax/3. views/3. Decorators/1. sections/examples/registered/index.js
+++ articles/syntax/3. views/3. Decorators/1. sections/examples/registered/index.js
@@ -1,11 +1,11 @@
 var Application = require("mojo-application");
 
 module.exports = Application.extend({
-  registerPlugins: function () {
-    this.use(require("mojo-views"));
-    this.use(require("./views"));
-    this.use(require("mojo-paperclip"));
-  },
+  plugins: [
+    require("mojo-views"),
+    require("./views"),
+    require("mojo-paperclip")
+  ],
   initialize: function (options) {
     options.element.appendChild(this.views.create("main").render());
   }
